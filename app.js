7184f52b7c1f1e4b173dd9369fca8402c773218e
Anjali Pillai <200377480@student.georgianc.on.ca>
2019-06-05 13:15:23 -0400

    gitignore file added

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(`hey 'dere world!`);
-});
+//our routes
+const routes = require('./routes.js');
+app.use('/', routes);
 
-app.get('/about', (req, res)=> {
-    res.send(` i like long walks on the beach.`);
-});
+// view configurtaion
+const path = require('path');
+app.set('views', path.join_dirname, 'views');
+app.set('view engine', 'pug');
 
 const port= process.env.PORT || 4000;
-
-
 app.listen(port, ()=> console.log(`listening on port ${port}`));
 
